e873b44440a52cab4441feceb1793f8cdbb2f491
unknown <windelle@xpay.world>
2020-02-18 13:10:24 +0800

    added AirFi requested methods

--- xpaysdk/src/main/java/com/xpayworld/sdk/payment/network/RetrofitClient.kt
+++ xpaysdk/src/main/java/com/xpayworld/sdk/payment/network/RetrofitClient.kt
@@ -27,78 +27,78 @@ class RetrofitClient
 //                .add(ApiConstants.API_HOST,
 //                        "sha256/4hw5tz+scE+TW+mlai5YipDfFWn1dqvfLG+nU7tq1V8=")
 //                .build()
-//        val interceptor = HttpLoggingInterceptor()
-//        interceptor.level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
-//
-//        val okHttpClient = OkHttpClient.Builder()
-//                .connectTimeout(30, TimeUnit.SECONDS)
-//                .writeTimeout(30, TimeUnit.SECONDS)
-////                .certificatePinner(certPinner)
-//                .readTimeout(30, TimeUnit.SECONDS)
-//                .addInterceptor(interceptor).build()
-//
-//        return Retrofit.Builder()
-//            .baseUrl(ApiConstants.API_HOST)
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io())).client(okHttpClient)
-//            .build()
-//================================================================================================================//
-//============================================ connection with SSL ===============================================//
-//================================================================================================================//
-        // https://gist.github.com/maiconhellmann/c61a533eca6d41880fd2b3f8459c07f7
-        // https://stackoverflow.com/questions/37686625/disable-ssl-certificate-check-in-retrofit-library
-
-        var okHttpClient:OkHttpClient;
         val interceptor = HttpLoggingInterceptor()
         interceptor.level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
 
-        try {
-            // Create a trust manager that does not validate certificate chains
-            val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
-                @Throws(CertificateException::class)
-                override fun checkClientTrusted(chain: Array<java.security.cert.X509Certificate>, authType: String) {
-                }
-
-                @Throws(CertificateException::class)
-                override fun checkServerTrusted(chain: Array<java.security.cert.X509Certificate>, authType: String) {
-                }
-
-                override fun getAcceptedIssuers(): Array<java.security.cert.X509Certificate> {
-                    return arrayOf()
-                }
-            })
-
-            // Install the all-trusting trust manager
-            val sslContext = SSLContext.getInstance("SSL")
-            sslContext.init(null, trustAllCerts, java.security.SecureRandom())
-            // Create an ssl socket factory with our all-trusting manager
-            val sslSocketFactory = sslContext.socketFactory
-
-            val hostnameVerifier = HostnameVerifier { _, session ->
-                //                HttpsURLConnection.getDefaultHostnameVerifier().run {
-//                    verify(ApiConstants.API_HOST, session)
-//                }
-                true
-            }
-
-            okHttpClient = OkHttpClient.Builder()
-                .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
-                .hostnameVerifier(hostnameVerifier)
+        val okHttpClient = OkHttpClient.Builder()
                 .connectTimeout(30, TimeUnit.SECONDS)
                 .writeTimeout(30, TimeUnit.SECONDS)
+//                .certificatePinner(certPinner)
                 .readTimeout(30, TimeUnit.SECONDS)
-                .addInterceptor(interceptor)
-                .build()
-
-
-        } catch (e: Exception) {
-            throw RuntimeException(e)
-        }
+                .addInterceptor(interceptor).build()
 
         return Retrofit.Builder()
             .baseUrl(BuildConfig.API_HOST)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io())).client(okHttpClient)
             .build()
+//================================================================================================================//
+//============================================ connection with SSL ===============================================//
+//================================================================================================================//
+        // https://gist.github.com/maiconhellmann/c61a533eca6d41880fd2b3f8459c07f7
+        // https://stackoverflow.com/questions/37686625/disable-ssl-certificate-check-in-retrofit-library
+
+//        var okHttpClient:OkHttpClient;
+//        val interceptor = HttpLoggingInterceptor()
+//        interceptor.level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+//
+//        try {
+//            // Create a trust manager that does not validate certificate chains
+//            val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+//                @Throws(CertificateException::class)
+//                override fun checkClientTrusted(chain: Array<java.security.cert.X509Certificate>, authType: String) {
+//                }
+//
+//                @Throws(CertificateException::class)
+//                override fun checkServerTrusted(chain: Array<java.security.cert.X509Certificate>, authType: String) {
+//                }
+//
+//                override fun getAcceptedIssuers(): Array<java.security.cert.X509Certificate> {
+//                    return arrayOf()
+//                }
+//            })
+//
+//            // Install the all-trusting trust manager
+//            val sslContext = SSLContext.getInstance("SSL")
+//            sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+//            // Create an ssl socket factory with our all-trusting manager
+//            val sslSocketFactory = sslContext.socketFactory
+//
+//            val hostnameVerifier = HostnameVerifier { _, session ->
+//                //                HttpsURLConnection.getDefaultHostnameVerifier().run {
+////                    verify(ApiConstants.API_HOST, session)
+////                }
+//                true
+//            }
+//
+//            okHttpClient = OkHttpClient.Builder()
+//                .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+//                .hostnameVerifier(hostnameVerifier)
+//                .connectTimeout(30, TimeUnit.SECONDS)
+//                .writeTimeout(30, TimeUnit.SECONDS)
+//                .readTimeout(30, TimeUnit.SECONDS)
+//                .addInterceptor(interceptor)
+//                .build()
+//
+//
+//        } catch (e: Exception) {
+//            throw RuntimeException(e)
+//        }
+//
+//        return Retrofit.Builder()
+//            .baseUrl(BuildConfig.API_HOST)
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io())).client(okHttpClient)
+//            .build()
     }
 }
